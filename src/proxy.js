de8ace6701dbd360db5f77d286c1c6f01066a877
Viktor Melnyk <Viktor_Melnyk@epam.com>
2019-04-05 13:16:55 +0300

    uses questinator-connector instead of local-tunnel

--- src/proxy.js
+++
@@ -1,5 +0,0 @@
-const httpProxy = require('http-proxy');
-const { proxyPort, port } = require("./contender.json");
-httpProxy
-    .createProxyServer({target: `http://localhost:${port}`})
-    .listen(proxyPort);
